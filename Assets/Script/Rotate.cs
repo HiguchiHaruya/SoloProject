1c1c59403a70c31404f238bf173159a71bb3da7e
HiguchiHaruya <134475780+HiguchiHaruya@users.noreply.github.com>
2023-11-25 00:36:33 +0900

    Update Rotate.cs
    ハンマーの回転の追加

--- Assets/Script/Rotate.cs
+++ Assets/Script/Rotate.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody))]
 public class Rotate : MonoBehaviour
 {
     float _scroll = 15f;
+    float _moveSpeed = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +17,8 @@ public class Rotate : MonoBehaviour
     void Update()
     {
         this.gameObject.transform.Rotate(Vector3.down * _scroll);
+        float horizon = -Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(horizon, 0, 0);
+        GetComponent<Rigidbody>().velocity = movement * _moveSpeed;
     }
 }
