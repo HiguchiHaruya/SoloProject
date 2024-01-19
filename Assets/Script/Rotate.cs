7d371c7c5cd5ec78bd971e3ec84efb10d8f4f6a3
HiguchiHaruya <134475780+HiguchiHaruya@users.noreply.github.com>
2024-01-19 16:34:46 +0900

    年末年始の分

--- Assets/Script/Rotate.cs
+++ Assets/Script/Rotate.cs
@@ -10,37 +10,37 @@ public class Rotate : MonoBehaviour
     [HideInInspector] public int _rotateSpeed = 3;
     void Start()
     {
-        _playerContr = FindObjectOfType<Playercontroller>();
+        //_playerContr = FindObjectOfType<Playercontroller>();
     }
     public int ChangeSpeed(int combo)
     {
         if (combo > 15 && combo < 30)
         {
             _speedup.Play();
-            return 10;
+            return 15;
         }
         if (combo > 30 && combo < 50)
         {
             _speedup.Play();
-            return 15;
+            return 17;
         }
         if (combo > 50)
         {
             _speedup.Play();
             return 20;
         }
-        return 3;
+        return 13;
     }
     public void Update()
     {
-        if (_playerContr != null)
-        {
-            Debug.Log(_playerContr._combo);
-            gameObject.transform.Rotate(Vector3.up * ChangeSpeed(_playerContr._combo));
+        //if (_playerContr != null)
+        //{
+            //Debug.Log(_playerContr._combo);
+            gameObject.transform.Rotate(Vector3.up * 14);
             float horizon = -Input.GetAxis("Horizontal");
             Vector3 movement = new Vector3(horizon, 0, 0);
             GetComponent<Rigidbody>().velocity = movement * 2;
-        }
-        else if (_playerContr == null) Debug.Log("Null‚Å‚·");
+        //}
+         if (_playerContr == null) Debug.Log("Null‚Å‚·");
     }
 }
